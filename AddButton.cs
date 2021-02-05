8b1851deb66504b8e65169d1384cbd7b099e642a
AnroiDevel <AnroiDevel@gmail.com>
2021-02-05 16:40:10 +0300

    Revert "test"
    This reverts commit 5d5f83bee9f9a981b4ec79bcecb2b9b7b74cbf34.

--- AddButton.cs
+++ AddButton.cs
@@ -13,26 +13,9 @@ namespace ProgectName
     {
 
 
-
         public AddButton()
         {
             InitializeComponent();
-            textBox1.KeyDown += AddButton_KeyDown;
-            textBox2.KeyDown += AddButton_KeyDown;
-        }
-
-        private void AddButton_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter && textBox2.Text.Length > 0 && textBox1.Text.Length > 0)
-            {
-                button_OK.PerformClick();
-            }
-            else if (e.KeyCode == Keys.Enter && textBox1.Text.Length > 0)
-            {
-                textBox2.Focus();
-
-            }
-
         }
 
         public delegate void CustomEventHandler(object sender, EventArgs e);
@@ -43,13 +26,13 @@ namespace ProgectName
             //int code;
             //int.TryParse(textBox2.Text, out code);
             string buttonName = Functions.CreateButtonName(FileFunctions.NumberOfButtonsInConfigFile());
-            OnRaiseCustomEvent(new EventArgs());
+            OnRaiseCustomEvent(new EventArgs() );
 
             FileFunctions.Write(textBox1.Text + "\t" + buttonName);
 
             Close();
         }
-
+        
         protected virtual void OnRaiseCustomEvent(EventArgs e)
         {
             // Make a temporary copy of the event to avoid possibility of
@@ -61,13 +44,13 @@ namespace ProgectName
             if (raiseEvent != null)
             {
                 // Format the string to send inside the CustomEventArgs parameter
-
+                
 
                 // Call to raise the event.
                 raiseEvent(this, e);
             }
         }
-
+        
         private void button_Cancel_Click(object sender, EventArgs e)
         {
             Close();
